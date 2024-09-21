1db1f1ec1286fa145ca759fb950f6e6971e90df1
Muskan <102637258+muskan025@users.noreply.github.com>
2024-09-21 23:44:26 +0530

    Update index.js

--- blogapp_server/index.js
+++ blogapp_server/index.js
@@ -27,7 +27,7 @@ const store = new mongoDbSession({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://liveup-blog-webapp-frontend.vercel.app', 
   credentials: true
 }));
 app.use(fileUpload({
