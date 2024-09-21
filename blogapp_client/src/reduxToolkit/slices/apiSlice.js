6d1abdd28e13494443c1f27d3f2f1c093f293898
Muskan <102637258+muskan025@users.noreply.github.com>
2024-09-21 23:39:08 +0530

    Update apiSlice.js

--- blogapp_client/src/reduxToolkit/slices/apiSlice.js
+++ blogapp_client/src/reduxToolkit/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8000",
+        baseUrl: "https://liveup-blog-webapp.vercel.app",
         credentials: 'include' }),
         tagTypes: ["AllBlogs","FollowList"],
     endpoints: (builder) => ({
