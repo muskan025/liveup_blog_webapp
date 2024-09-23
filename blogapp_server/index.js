c87f423bb46c2bf55688dbe74a7673f5a39d72ec
Muskan <102637258+muskan025@users.noreply.github.com>
2024-09-23 16:03:55 +0530

    Update index.js

--- blogapp_server/index.js
+++ blogapp_server/index.js
@@ -44,10 +44,11 @@ app.use(
     store: store,
     cookie: { 
       httpOnly: true,
-     secure: process.env.NODE_ENV === 'production',
+     secure: true,
      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24,
-      path: "/"
+      path: "/",
+     domain: process.env.COOKIE_DOMAIN
     },
   })
 );
