aa860af9a0e971edccc3633c642d2df718ceb155
Muskan <102637258+muskan025@users.noreply.github.com>
2024-09-24 10:21:07 +0530

    Update cookie config

--- blogapp_server/index.js
+++ blogapp_server/index.js
@@ -44,8 +44,8 @@ app.use(
     store: store,
     cookie: { 
       httpOnly: true,
-     secure: true,
-     sameSite: "none",
+     secure: process.env.NODE_ENV === 'production',
+     sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24,
       path: "/",
      domain: process.env.COOKIE_DOMAIN
