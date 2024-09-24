ac1da9081e436934b6266ee8ce5494276c8fd9fc
Muskan <102637258+muskan025@users.noreply.github.com>
2024-09-24 11:28:08 +0530

    Update image URL

--- blogapp_client/src/pages/SingleBlog.jsx
+++ blogapp_client/src/pages/SingleBlog.jsx
@@ -9,7 +9,7 @@ const SingleBlog = () => {
 
   const { state } = useLocation()
   const { _id, title, thumbnail, readTime, textBody, likesCount,likes, userId } = state
-   const profileImage = `https://liveup-blog-webapp.vercel.app/${userId.profileImg}`
+   const profileImage = `https://liveup-api.vercel.app/${userId.profileImg}`
 
  
    return (
