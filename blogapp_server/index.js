6b2e5bf1d131c7c3976c3449166324be2d9481cb
Muskan <102637258+muskan025@users.noreply.github.com>
2024-10-10 13:15:35 +0530

    Update CORS origin

--- blogapp_server/index.js
+++ blogapp_server/index.js
@@ -26,7 +26,7 @@ const store = new mongoDbSession({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: 'https://liveup.vercel.app',  
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
